3ef459534158944f9d1567313abd5c49cdacd204
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 13:27:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
-var playlist = new Object({Alanis Morisette: 'Ironic', Jewel: "You were meant for me"});
+var playlist = new Object({});
 
 function updatePlaylist(playlist, artist, songTitle) {
   return Object.assign({}, playlist, {[artist]: songTitle} )
 }
+
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist.artist;
